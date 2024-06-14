4115b4cde1246c4b81448a9218a89fed058ab224
Rochdyath <rochdiath.bachabi@epitech.eu>
2024-06-14 23:19:37 +0200

    feat: implementation of keyboard interpretor

--- src/KeyboardKeyInterpretor.py
+++ src/KeyboardKeyInterpretor.py
@@ -1,10 +1,42 @@
 from BotDirective import BotDirective
 from IDirectiveInterpretor import IDirectiveInterpretor
+from time import time
+from pynput import keyboard
+
+class KeyboardController:
+    def __init__(self):
+        self.current_dir = BotDirective.NOTHING
+    
+    def on_press(self, key):
+        if key == keyboard.Key.up:
+            self.current_dir = BotDirective.GO_FORWARD
+        if key == keyboard.Key.down:
+            self.current_dir = BotDirective.GO_BACKWARD
+        if key == keyboard.Key.left:
+            self.current_dir = BotDirective.TURN_LEFT
+        if key == keyboard.Key.right:
+            self.current_dir = BotDirective.TURN_RIGHT
+
+    def on_release(self, key):
+        if key == keyboard.Key.up:
+            self.current_dir = BotDirective.NOTHING
+        if key == keyboard.Key.down:
+            self.current_dir = BotDirective.NOTHING
+        if key == keyboard.Key.left:
+            self.current_dir = BotDirective.NOTHING
+        if key == keyboard.Key.right:
+            self.current_dir = BotDirective.NOTHING
+
 
 @IDirectiveInterpretor.register
 class KeyboardKeyInterpretor(IDirectiveInterpretor):
     def __init__(self):
-        pass
+        self.controller = KeyboardController()
+        self.listener = keyboard.Listener(
+            on_press=self.controller.on_press,
+            on_release=self.controller.on_release)
+        self.listener.start()
 
-    def poll_directive(self):
-        pass
+    def poll_directive(self) -> BotDirective:
+        if self.listener.is_alive():
+            return self.controller.current_dir
