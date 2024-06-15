e41abbad71eb82e914b1b3632ac88996179c1ff0
Rochdyath <rochdiath.bachabi@epitech.eu>
2024-06-15 13:00:42 +0200

    feat: document create_instances and config method of MainApp class

--- src/MainApp.py
+++ src/MainApp.py
@@ -44,6 +44,14 @@ class MainApp(metaclass=MainAppMeta):
         self._directive_interpretor = value
 
     def create_instannces(self, robot, control):
+        """
+        Cette fonction initialise les attributs bot_connector et directive_interpretor selon les paramètres robot et control.
+        
+        :param robot: chaîne de caractères variant entre "MBOT2" et "ILO Robot"
+        :type robot: str
+        :param control: chaîne de caractères variant entre "Hand" et "Keyboard"
+        :type control: str
+        """
         if robot == "ILO Robot": self.bot_connector = IloRobotConnector()
         else: self.bot_connector = MbotConnector()
 
@@ -55,6 +63,9 @@ class MainApp(metaclass=MainAppMeta):
         return (robot, control)
 
     def config(self):
+        """
+        Cette fonction initialise les attributs bot_connector et directive_interpretor selon la configuration dans le fichier config.json.
+        """
         file_path = "config.json"
         try:
             with open(file_path, 'r') as file:
