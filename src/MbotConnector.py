8fcc4d150b4b11ba5f9e98ad5700273cc94cf1e7
Rochdy-max <rochdy.bachabi@epitech.eu>
2024-09-24 21:38:56 +0200

    implement MbotConnector using cyberpi module as base

--- src/MbotConnector.py
+++ src/MbotConnector.py
@@ -3,17 +3,40 @@ from IBotConnector import IBotConnector
 
 @IBotConnector.register
 class MbotConnector(IBotConnector):
+    _move_speed = 50
+    _stop_speed = 0
+
     def __init__(self):
         pass
 
     def connect(self):
-        pass
+        import cyberpi
+        self.cyberpi = cyberpi
     
     def disconnect(self):
-        pass
+        # Disconnect
+        self.cyberpi.stop_all()
     
-    def send_directive(self):
-        pass
+    def send_directive(self, directive):
+        match directive:
+            case BotDirective.NOTHING:
+                # Nothing to do
+                pass
+            case BotDirective.GO_FORWARD:
+                # Go Forward
+                self.cyberpi.mbot2.forward(speed=self._move_speed)
+            case BotDirective.GO_BACKWARD:
+                # Go Backward
+                self.cyberpi.mbot2.backward(speed=self._move_speed)
+            case BotDirective.TURN_LEFT:
+                # Turn Left
+                self.cyberpi.mbot2.turn_left(speed=self._move_speed)
+            case BotDirective.TURN_RIGHT:
+                # Turn Right
+                self.cyberpi.mbot2.turn_right(speed=self._move_speed)
+            case BotDirective.STOP:
+                # No movement
+                self.cyberpi.mbot2.forward(speed=self._stop_speed)
     
     def get_state(self):
         pass
